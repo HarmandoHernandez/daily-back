8de4878686fd86a822684d4f5e92648544f74489
HarmandoHernandez <josearmandohv99@gmail.com>
2022-02-14 19:30:00 -0600

    Initial deploy

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
 const { dbConnection } = require('./db/config')
+const path = require('path')
 
 // Crear el servidor/aplicación de express
 const app = express()
@@ -20,6 +21,11 @@ app.use(express.json())
 
 // Rutas
 app.use('/api/auth', require('./routes/auth'))
+
+// Rutas para el FrontEnd. Sin #
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'public/index.html'))
+})
 
 app.listen(process.env.PORT, () => {
   console.log(`Servidor corriendo en puerto ${process.env.PORT}`)
