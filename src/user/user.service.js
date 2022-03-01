d83c91b364cec449b9ed6726c5ae4497c4bfa816
Armando <luis.armando.store@gmail.com>
2022-03-01 13:36:42 -0600

    Defined types in User Service

--- src/user/user.service.js
+++ src/user/user.service.js
@@ -3,25 +3,34 @@ const bcrypt = require('bcryptjs')
 
 // Customs
 const USER_PARAMS = require('./user.enum')
+const STATUS = require('../shared/enums/status.enum')
+const VALIDATORS = require('../shared/enums/validators.enum')
 const UserDAL = require('./user.dal')
 const getAnErrorResponse = require('../shared/helpers/responses/error.response')
-const VALIDATORS = require('../shared/enums/validators.enum')
 const GeneralFormat = require('../shared/helpers/responses/general.format')
-const STATUS = require('../shared/enums/status.enum')
+// eslint-disable-next-line no-unused-vars
+const UserFormat = require('../shared/helpers/responses/user.format')
+
 // Instances
 const userDal = new UserDAL()
 
 class UserService {
+  /**
+   * Logic and format to create new User
+   * @param {UserFormat} userData User data
+   * @returns {Promise<GeneralFormat>} Response format
+   */
   async createUser (userData) {
     try {
+      // Encript password
       const salt = bcrypt.genSaltSync()
       userData.password = bcrypt.hashSync(userData.password, salt)
-
+      // Save user
       const userDb = await userDal.createOne(userData)
-      if (userDb === null) {
-        return getAnErrorResponse(VALIDATORS.CORRUPT, USER_PARAMS.USER)
+      if (userDb !== null) {
+        return new GeneralFormat(STATUS.SUCCESS, userDb)
       }
-      return new GeneralFormat(STATUS.SUCCESS, userDb)
+      return getAnErrorResponse(VALIDATORS.CORRUPT, USER_PARAMS.USER)
     } catch (error) {
       return getAnErrorResponse(VALIDATORS.FATAL_ERROR, 'USER:CREATE', error)
     }
@@ -44,33 +53,54 @@ class UserService {
     }
   }
 
+  /**
+   * Logic and format to Get User by email
+   * @param {string} email User email
+   * @returns {Promise<GeneralFormat>} Response format
+   */
   async getOneByEmail (email) {
     try {
       const userDb = await userDal.getByEmail(email)
-      if (userDb === null) {
-        return getAnErrorResponse(VALIDATORS.NOEXIST, USER_PARAMS.USER)
+      if (userDb !== null) {
+        return new GeneralFormat(STATUS.SUCCESS, userDb)
       }
-      return new GeneralFormat(STATUS.SUCCESS, userDb)
+      return getAnErrorResponse(VALIDATORS.NOEXIST, USER_PARAMS.USER)
     } catch (error) {
       return getAnErrorResponse(VALIDATORS.FATAL_ERROR, 'USER:GETBYEMAIL', error)
     }
   }
 
+  /**
+   * Logic to include activity reference in user
+   * @param {string} userId User identification
+   * @param {string} activityId Activity identification
+   * @returns {Promise<GeneralFormat>} Response format
+   */
   async includeActivity (userId, activityId) {
     try {
       const userDb = await userDal.includeActivity(userId, activityId)
-      // TODO: Validar que no sea null
-      return new GeneralFormat(STATUS.SUCCESS, userDb)
+      if (userDb !== null) {
+        return new GeneralFormat(STATUS.SUCCESS, userDb)
+      }
+      return getAnErrorResponse(VALIDATORS.NOEXIST, USER_PARAMS.USER)
     } catch (error) {
       return getAnErrorResponse(VALIDATORS.FATAL_ERROR, 'USER:INCLUDACTIVITY', error)
     }
   }
 
+  /**
+   * Logic to remove activity reference from user
+   * @param {string} userId User identification
+   * @param {string} activityId Activity identification
+   * @returns {Promise<GeneralFormat>} Response format
+   */
   async removeActivity (userId, activityId) {
     try {
       const userDb = await userDal.removeActivity(userId, activityId)
-      // TODO: Validar que no sea null
-      return new GeneralFormat(STATUS.SUCCESS, userDb)
+      if (userDb !== null) {
+        return new GeneralFormat(STATUS.SUCCESS, userDb)
+      }
+      return getAnErrorResponse(VALIDATORS.NOEXIST, USER_PARAMS.USER)
     } catch (error) {
       return getAnErrorResponse(VALIDATORS.FATAL_ERROR, 'USER:UPDATE', error)
     }
